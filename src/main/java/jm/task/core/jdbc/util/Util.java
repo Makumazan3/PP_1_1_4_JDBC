b7f9154d664ce4c5d13441ef494212744b753b6c
Bolotov Alexander <distalker1@gmail.com>
2022-01-17 13:52:50 +0300

    I correct mistakes.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,30 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    //   создаём переменные
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/mysql";
+    private static final String DB_USERNAME = "Makumazan";
+    private static final String DB_PASSWORD = "root";
+
+    // создаём соединение
+    public static Connection getConnection() {
+        Connection connection = null;
+        // если вылетят ошибки
+
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection ERROR");
+        }
+        return connection;
+    }
 }
